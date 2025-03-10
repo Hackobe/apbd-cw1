2fccc046eff78e6754c989f4b66b46aeb576125e
Jakub Brochocki <s27343@pjwstk.edu.pl>
2025-03-10 14:42:59 +0100

    Modyfikacja 2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more informationConsole.WriteLine("Witaj, świecie!");
 
 static void Powitanie(){
-Console.WriteLine("Kolejna zmiana kodu (2)")
+Console.WriteLine("Ostatnia zmiana kodu (3)")
 }
 
 Powitanie();
